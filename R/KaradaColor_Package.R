34e58d6fedd0266398c14bc1618157f1e71f1c37
KaradaGood <info@karada-good.net>
2023-04-08 12:53:52 +0900

    23.04.08

---
+++ R/KaradaColor_Package.R
@@ -0,0 +1,49 @@
+#' @title Color palette dataset for the KaradaColor Package
+#' @description \code{df_KG_palettes} Displays the color palette dataset
+#' @return Vector containing a hex color code and palette name
+#' @usage df_KG_palettes
+#' @keywords data
+#' @name df_KG_palettes
+#' @docType data
+#' @examples
+#' data("df_KG_palettes")
+#' head(df_KG_palettes)
+"df_KG_palettes"
+
+
+#' @title Displays the color palette of the KaradaColor Package
+#' @description \code{Show_KG_Palette} Displays the color palette of the KaradaColor Package
+#' @importFrom grDevices colorRampPalette rgb
+#' @importFrom graphics image rect text
+#' @param name color palette name
+#' @param n Number of colors to create
+#' @param showPalette Plotting Color Palette
+#' @return Vector containing a hex color code representation for the chosen palette
+#' @export Show_KG_Palette
+#' @examples
+#' Show_KG_Palette("Amanohashidate", n = 5, showPalette = TRUE)
+
+Show_KG_Palette <- function(name, n = 5,
+                            showPalette = TRUE) {
+  #Check the color palette name
+  pal_name <- df_KG_palettes[[name]]
+  #Displays the available color palette name if it is outside of the recorded name, and then exits
+  if (is.null(pal_name)) {
+    stop("Palette not found.\nCheck the color palette name with the 'ALL_KG_Palette' command.")
+  }
+  #Create color palette
+  pal_col <- colorRampPalette(pal_name)
+
+  if(showPalette == TRUE){
+  ###Plot Preparation#####
+  image(x = seq_len(n), y = 1, z = as.matrix(seq_len(n)),
+        col = pal_col(n), xlab = "", ylab = "",
+        xaxt = "n", yaxt = "n", bty = "n")
+  rect(0, 0.9, n + 1, 1.1, col = rgb(1, 1, 1, 0.8), border = NA)
+  text(labels = name, x = (n + 1)/2, y = 1, cex = 2)}
+
+  return(pal_col(n))
+
+}
+
+hihi
