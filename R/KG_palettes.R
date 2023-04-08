5737fae388fc879eda6e6676f16963f204a2e813
KaradaGood <info@karada-good.net>
2023-04-08 14:51:42 +0900

    23.04.08

--- R/KG_palettes.R
+++ R/KG_palettes.R
@@ -2,5 +2,13 @@
 KG_palettes <- list(
   'Haru_Sonsho-In' = c("#d3b5b3", "#fadfe6", "#f7d0a7", "#d2d972", "#495642"),
   'Haru_Kyoto_Ine-cho' = c("#efffff", "#b8d3e8", "#214458", "#30424c", "#3b413f"),
-  'Amanohashidate' = c("#869ea2", "#74887f", "#435047", "#8fa6ae", "#98afb7")
+  'Amanohashidate' = c("#869ea2", "#74887f", "#435047", "#8fa6ae", "#b6b9a6"),
+  'Hanamushiro' = c("#d6c3e1", "#dccada", "#e1bed6", "#c27ba3", "#ab799c"),
+  'Hokkaido_Fry' = c("#bd9267", "#594433", "#9a9da4", "#90b8dc", "#5b84bc"),
+  'Yozakura' = c("#dad4d6", "#6b6952", "#c8bfb6", "#827d67", "#eae9e7"),
+  'Kyoto_City' = c("#98a2b1", "#8799b1", "#abb0b6", "#8aa9d8", "#7490c2"),
+  'Hakodate_Mt' = c("#6c6c6a", "#542d1c", "#726129", "#8a5e37", "#d4b694"),
+  'Akashiyaki' = c("#a71426", "#704207", "#c6b395", "#ab824c", "#660809"),
+  'Sapporo_Sta' = c("#7f776c", "#88745b", "#c4beb2", "#3c4c6d", "#4875b6"),
+  'Otaru_Unga' = c("#7d5c53", "#54514a", "#466265", "#98a1a8", "#c3ced4"),
 )
