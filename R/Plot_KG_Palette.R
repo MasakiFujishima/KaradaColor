f6e979393d8180aea17732d83c18d71c9f56d0a6
KaradaGood <info@karada-good.net>
2023-04-17 20:13:31 +0900

    23.04.17

--- R/Plot_KG_Palette.R
+++ R/Plot_KG_Palette.R
@@ -34,12 +34,6 @@ plot_KG_palette <- function(name = "Sapporo_Sta", color = NULL, n = 5) {
 
   }else{
 
-    if (!is.data.frame(color)) {
-
-      stop("データフレームではない")
-
-    }
-
     color_data <- color[[1]]
     n <- length(color_data)
     image(x = seq_len(n), y = 1, z = as.matrix(seq_len(n)),
@@ -51,3 +45,51 @@ plot_KG_palette <- function(name = "Sapporo_Sta", color = NULL, n = 5) {
 
   }
 }
+
+#############
+#テスト
+#########################################################
+################################################
+#x <- KG_create_tc()
+#reverse <- nrow(x):1
+
+#image(x = seq_len(nrow(x)), y = seq_len(ncol(x)),
+#      z = t(matrix(data = seq_len(ncol(x)*nrow(x)),
+#                   nrow = ncol(x), ncol = nrow(x))),
+#      col = as.matrix(x), xlab = "", ylab = "",
+#      xaxt = "n", yaxt = "n", bty = "n")
+#rect(xleft = 0, ybottom = 0.9, xright = nrow(x) + 1, ytop = 1.1,
+#     col = rgb(1, 1, 1, 0.8), border = NA)
+#text(labels = names(x), x = (nrow(x) + 1)/2, y = 1, cex = 2)
+#return(color_data)
+###################################################
+################################################
+show_KG_palette(KG_create_cc(c("#ff48ac", "red")))
+#複数行データ
+x <- KG_create_cc(c("#ff48ac", "red"))
+#1行データ
+x <- KG_create_tc()
+
+#複数行データ
+yreverse <- nrow(x):1
+x <- t(as.matrix(x)[yreverse, ])
+#1行データ
+x <- t(as.matrix(x))
+
+image(x = seq_len(ncol(x)), y = seq_len(nrow(x)),
+      z = matrix(data = seq_len(ncol(x)*nrow(x)),
+                 nrow = ncol(x), ncol = nrow(x)),
+      col = x, xlab = "", ylab = "",
+      xaxt = "n", yaxt = "n", bty = "n")
+
+##行数データで処理を変える#####
+rect(xleft = 0, ybottom = 0.9, xright = nrow(x) + 1, ytop = 1.1,
+     col = rgb(1, 1, 1, 0.8), border = NA)
+
+rect(xleft = 0, ybottom = 1.9, xright = nrow(x) + 1, ytop = 2.1,
+     col = rgb(1, 1, 1, 0.8), border = NA)
+
+text(labels = names(x)[1], x = (nrow(x) + 1)/3, y = c(2:1), cex = 2)
+
+return(color_data)
+###################################################
