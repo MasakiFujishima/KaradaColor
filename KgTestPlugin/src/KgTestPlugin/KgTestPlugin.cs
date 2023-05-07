95c5655b1855c2d2983f0b0c5077c305223cf796
MasakiFujishima <info@karada-good.net>
2023-05-07 14:42:00 +0900

    23.05.07

---
+++ KgTestPlugin/src/KgTestPlugin/KgTestPlugin.cs
@@ -0,0 +1,27 @@
+namespace Loupedeck.KgTestPlugin
+{
+    using System;
+
+    // This class contains the plugin-level logic of the Loupedeck plugin.
+
+    public class KgTestPlugin : Plugin
+    {
+        // Gets a value indicating whether this is an Universal plugin or an Application plugin.
+        public override Boolean UsesApplicationApiOnly => true;
+
+        // Gets a value indicating whether this is an API-only plugin.
+        public override Boolean HasNoApplication => true;
+
+        public KgTestPlugin() => PluginLog.Init(this.Log);
+
+        // This method is called when the plugin is loaded during the Loupedeck service start-up.
+        public override void Load()
+        {
+        }
+
+        // This method is called when the plugin is unloaded during the Loupedeck service shutdown.
+        public override void Unload()
+        {
+        }
+    }
+}
